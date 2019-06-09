92af6ae215e5de4f66a00fa0e5bd5d5006e841e5
José Coelho <camilocoelho93@gmail.com>
2019-06-09 22:19:06 +0100

    Add class on click

--- src/components/elements/NavBar.js
+++ src/components/elements/NavBar.js
@@ -5,43 +5,57 @@ import MenuItem from "./MenuItem"
 //document.querySelector('.nav-menu').classList.toggle('is-active')
 //this.classList.toggle('is-active')
 
-function NavBar() {
-    return(
-        <nav className="navbar is-transparent">
-            <div className="container">
-                <div className="navbar-brand">
-                    <a href="/" className="navbar-item title is-unselectable my-name">
-                        José Coelho
-                    </a>
-                    <span className="navbar-burger burger">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </span>
-                </div>
-                <div className="navbar-menu nav-menu">
-                    <div className="navbar-end">
-                        <MenuItem
-                            text = "About Me"
-                            href = "#aboutMe"
-                        />
-                        <MenuItem
-                            text = "Skills"
-                            href = "#skills"
-                        />
-                        <MenuItem
-                            text = "Experience"
-                            href = "#experience"
-                        />
-                        <MenuItem
-                            text = "Articles"
-                            href = "#articles"
-                        />
+class NavBar extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {showMenu: false};
+        this.handleMenuClick = this.handleMenuClick.bind(this);
+    }
+
+    handleMenuClick(e) {
+        const currentState = this.state.showMenu;
+        this.setState({ showMenu: !currentState});
+    }
+
+    render(){
+        return(
+            <nav className="navbar is-transparent">
+                <div className="container">
+                    <div className="navbar-brand">
+                        <a href="/" className="navbar-item title is-unselectable my-name">
+                            José Coelho
+                        </a>
+                        <span className="navbar-burger burger" onClick={this.handleMenuClick}>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </span>
+                    </div>
+                    <div className={'navbar-menu nav-menu ' + (this.state.showMenu ? 'is-active' : null)}>
+                        <div className="navbar-end" onClick={this.handleMenuClick}>
+                            <MenuItem
+                                text = "About Me"
+                                href = "#aboutMe"
+                            />
+                            <MenuItem
+                                text = "Skills"
+                                href = "#skills"
+                            />
+                            <MenuItem
+                                text = "Experience"
+                                href = "#experience"
+                            />
+                            <MenuItem
+                                text = "Articles"
+                                href = "#articles"
+                            />
+                        </div>
                     </div>
                 </div>
-            </div>
-        </nav>
-    )
+            </nav>
+        );
+    }    
 }
 
 export default NavBar
