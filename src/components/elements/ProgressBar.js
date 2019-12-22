7cfe686c9ad2175f20651796c2e22c3f051aa7c1
José Coelho <camilocoelho93@gmail.com>
2019-12-22 19:36:01 +0100

    Fix skills percentage

--- src/components/elements/ProgressBar.js
+++ src/components/elements/ProgressBar.js
@@ -3,8 +3,8 @@ import React from "react"
 function ProgressBar(props) {
     return(
         <div>
-            <span className="title is-5">{props.text}</span>
-            <span className="subtitle is-6">{props.percentage}%</span>
+            <span className="title is-5 is-spaced">{props.text}</span>
+            <span className="subtitle is-6 skill-percentage">{props.percentage}%</span>
             <progress className="progress is-success" value={props.percentage} max="100">{props.percentage}%</progress>
         </div>
     )
