bcf56f23e48cb2e32a6d0f6fcf93c08e5ed398f0
Chris Staykov <cstaykov@uwaterloo.ca>
2021-01-08 00:22:13 -0500

    Debug

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
-## Delete all files from FTP server
+## Update files on FTP server
+
+pwd
+ls
+ls build
 
 lftp -u $FTP_USER,$FTP_PASS $FTP_HOST <<END_SCRIPT
 set ftp:ssl-allow no
 debug
-glob -a rm -r *
+glob -a rm -r *(D)
 mirror -R build .
 quit
 END_SCRIPT
