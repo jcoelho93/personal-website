6162254420a3082d5c014bb89600924229544e6b
José Coelho <16445494+jcoelho93@users.noreply.github.com>
2025-06-22 23:26:32 +0100

    Upgrade bulma

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-import { createRoot } from 'react-dom/client';
-import React from "react";
+import { createRoot } from "react-dom/client";
 import App from "./App";
-
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />)
+root.render(<App />);
